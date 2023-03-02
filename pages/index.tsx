d5406d83ccf7d1da6c9730e4767595dffafe6d30
jdkim <jdkim@jdkimui-MacBookAir.local>
2023-03-02 23:28:33 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {useState, useEffect} from 'react'
 import styles from '../styles/Home.module.css'
 import TelegramLoginButton, { TelegramUser } from 'telegram-login-button'
 
 const Home: NextPage = () => {
 
- 
+  const [login, setLogin] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -20,11 +21,14 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        {login ? <h1>Logged in</h1> :  
         <TelegramLoginButton
         botName="TonicLoungeBot"
         usePic={true}
-        dataOnauth={(user: TelegramUser) => console.log(user)}
-      />
+        dataOnauth={(user: TelegramUser) => {setLogin(true); console.log(user)}}
+      />}
+       
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
